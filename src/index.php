6eae694b6863d7155f86fcefc22820f3d6bbedfc
mugi72-sr <shimodarukina@outlook.jp>
2026-07-24 11:10:42 +0900

    practice/ へコードと README を追加

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<?php
+// src/index.php
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>はじめてのPHP</title>
+</head>
+<body>
+    <h1>PHPの世界へようこそ！</h1>
+    <p>PHPを使って、動的なコンテンツを表示してみましょう。</p>
+    <p>1+1は、<?php echo 1+1;?>です。</p>
+    <p>現在の日時は、<?php echo date("Y年m月d日 H時i分s秒");?>です。</p>
+</body>
+</html>
